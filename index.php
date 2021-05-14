dd36dd098f95c6ef9f5046444e6f1e432cee77d9
Aleksandr <sanekclubstr@mail.ru>
2021-05-15 01:03:56 +0300

    1

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <script type='text/javascript' async defer src='https://herokuappcom.push4site.com/sdk'></script>
 </head>
 <body>
 Моя закладка
