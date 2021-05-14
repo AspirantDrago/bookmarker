19552722463c815c36434e864fb992da52a5b7cc
Aleksandr <sanekclubstr@mail.ru>
2021-05-15 01:44:16 +0300

    2

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <script type='text/javascript' async defer src='https://herokuappcom.push4site.com/sdk'></script>
+<!--    <script type="text/javascript" src="//www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>-->
+<!--    <script type="text/javascript" src="/firebase_subscribe.js"></script>-->
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+        window.OneSignal = window.OneSignal || [];
+        OneSignal.push(function() {
+            OneSignal.init({
+                appId: "214345c2-acf3-484c-a440-bc8d68aa5ae8",
+            });
+        });
+    </script>
 </head>
 <body>
 Моя закладка
+<button type="button" id="subscribe">Включить уведомления</button>
 </body>
 </html>
