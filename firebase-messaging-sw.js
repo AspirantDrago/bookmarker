40b7f614486f8c72532b534cfd83f1ece2d6a2fa
Aleksandr <sanekclubstr@mail.ru>
2021-05-15 04:35:18 +0300

    4

--- firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: '999110945056'
-});
-
-const messaging = firebase.messaging();
--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-<!--    <script type="text/javascript" src="//www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>-->
-<!--    <script type="text/javascript" src="/firebase_subscribe.js"></script>-->
+    <script type="text/javascript" src="//www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
+    <script type="text/javascript" src="/firebase_subscribe.js"></script>
     <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
     <script>
         window.OneSignal = window.OneSignal || [];
@@ -16,7 +16,6 @@
     </script>
 </head>
 <body>
-Моя закладка
-<button type="button" id="subscribe">Включить уведомления</button>
+Шаблон
 </body>
 </html>
