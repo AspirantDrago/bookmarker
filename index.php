134cf78dde726476d4cd0ebe4cfeb0f8e6a67e30
Aleksandr <sanekclubstr@mail.ru>
2021-05-15 07:34:02 +0300

    11

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta content="width=device-width,minimum-scale=1.0,maximum-scale=1.0" name="viewport">
     <title>Bookmarker</title>
-        <script type="text/javascript" src="//www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
-        <script type="text/javascript" src="/firebase_subscribe.js"></script>
-        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-        <script>
-            window.OneSignal = window.OneSignal || [];
-            OneSignal.push(function() {
-                OneSignal.init({
-                    appId: "214345c2-acf3-484c-a440-bc8d68aa5ae8",
-                });
+    <script type="text/javascript" src="//www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
+    <script type="text/javascript" src="/firebase_subscribe.js"></script>
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+        window.OneSignal = window.OneSignal || [];
+        OneSignal.push(function() {
+            OneSignal.init({
+                appId: "214345c2-acf3-484c-a440-bc8d68aa5ae8",
             });
-        </script>
+        });
+    </script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -29,24 +29,24 @@
     <img src="icon_android.png" alt="" height="35px">
 </nav>
 
-<ul class="nav nav-tabs">
-    <li class="nav-item">
-        <a class="nav-link" href="#">Всё</a>
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+        <a class="nav-link active" id="alllist-tab" data-toggle="pill" href="#alllist" role="tab" aria-controls="pills-home" aria-selected="true">Всё</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Видео</a>
+    <li class="nav-item" role="presentation">
+        <a class="nav-link" id="videolist-tab" data-toggle="pill" href="#videolist" role="tab" aria-controls="pills-home" aria-selected="true">Видео</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Статьи</a>
+    <li class="nav-item" role="presentation">
+        <a class="nav-link" id="articlelist-tab" data-toggle="pill" href="#articlelist" role="tab" aria-controls="pills-home" aria-selected="true">Статьи</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Музыка</a>
+    <li class="nav-item" role="presentation">
+        <a class="nav-link" id="musiclist-tab" data-toggle="pill" href="#musiclist" role="tab" aria-controls="pills-home" aria-selected="true">Музыка</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Подкасты</a>
+    <li class="nav-item" role="presentation">
+        <a class="nav-link" id="podcastlist-tab" data-toggle="pill" href="#podcastlist" role="tab" aria-controls="pills-home" aria-selected="true">Подкасты</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Заметки</a>
+    <li class="nav-item" role="presentation">
+        <a class="nav-link" id="noteslist-tab" data-toggle="pill" href="#noteslist" role="tab" aria-controls="pills-home" aria-selected="true">Заметки</a>
     </li>
 </ul>
 
@@ -72,46 +72,102 @@
     </nav>
 
     <div class="container main-list">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">АНГЛИЙСКИЙ НА СЛУХ</h5>
-                <p class="card-text">Слушаем и разбираем речь Billie Eilish (Билли Эйлиш/Айлиш) на английском языке...</p>
-                <p class="card-text"><small class="text-muted">25 мин 34 сек</small></p>
-                <div class="row justify-content-between">
-                    <a href="https://www.youtube.com/watch?v=JtmZ2e6mDL4" class="btn btn-primary col-5">Открыть</a>
-                    <a href="#" class="btn btn-secondary col-5">Удалить</a>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="alllist" role="tabpanel" aria-labelledby="alllist-tab">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">АНГЛИЙСКИЙ НА СЛУХ</h5>
+                        <p class="card-text">Слушаем и разбираем речь Billie Eilish (Билли Эйлиш/Айлиш) на английском языке...</p>
+                        <p class="card-text"><small class="text-muted">25 мин 34 сек</small></p>
+                        <div class="row justify-content-between">
+                            <a href="https://www.youtube.com/watch?v=JtmZ2e6mDL4" class="btn btn-primary col-5">Открыть</a>
+                            <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                        </div>
+                    </div>
+                    <a href="https://www.youtube.com/watch?v=JtmZ2e6mDL4">
+                        <img src="https://img.youtube.com/vi/JtmZ2e6mDL4/maxresdefault.jpg" class="card-img-bottom" alt="...">
+                    </a>
                 </div>
-            </div>
-            <a href="https://www.youtube.com/watch?v=JtmZ2e6mDL4">
-                <img src="https://img.youtube.com/vi/JtmZ2e6mDL4/maxresdefault.jpg" class="card-img-bottom" alt="...">
-            </a>
-        </div>
-        <br>
+                <br>
 
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Введение в Data Science и машинное обучение</h5>
-                <p class="card-text">Курс знакомит слушателей с основами машинного обучения и рассчитан в первую...</p>
-                <div class="row justify-content-between">
-                    <a href="https://stepik.org/course/4852/promo" class="btn btn-primary col-5">Открыть</a>
-                    <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Введение в Data Science и машинное обучение</h5>
+                        <p class="card-text">Курс знакомит слушателей с основами машинного обучения и рассчитан в первую...</p>
+                        <div class="row justify-content-between">
+                            <a href="https://stepik.org/course/4852/promo" class="btn btn-primary col-5">Открыть</a>
+                            <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <br>
+                <br>
 
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Представление налоговой и бухгалтерской отчётности</h5>
-                <p class="card-text">ОБЩАЯ ИНФОРМАЦИЯ Налоговая отчетность включает в себя совокупность документов, ...</p>
-                <p class="card-text"><small class="text-muted">≈ 4 мин (8 987 знаков)</small></p>
-                <div class="row justify-content-between">
-                    <a href="https://www.nalog.ru/rn77/taxation/submission_statements/" class="btn btn-primary col-5">Открыть</a>
-                    <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Представление налоговой и бухгалтерской отчётности</h5>
+                        <p class="card-text">ОБЩАЯ ИНФОРМАЦИЯ Налоговая отчетность включает в себя совокупность документов, ...</p>
+                        <p class="card-text"><small class="text-muted">≈ 4 мин (8 987 знаков)</small></p>
+                        <div class="row justify-content-between">
+                            <a href="https://www.nalog.ru/rn77/taxation/submission_statements/" class="btn btn-primary col-5">Открыть</a>
+                            <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                        </div>
+                    </div>
                 </div>
+                <br>
+            </div>
+            <div class="tab-pane fade" id="videolist" role="tabpanel" aria-labelledby="videolist-tab">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">АНГЛИЙСКИЙ НА СЛУХ</h5>
+                        <p class="card-text">Слушаем и разбираем речь Billie Eilish (Билли Эйлиш/Айлиш) на английском языке...</p>
+                        <p class="card-text"><small class="text-muted">25 мин 34 сек</small></p>
+                        <div class="row justify-content-between">
+                            <a href="https://www.youtube.com/watch?v=JtmZ2e6mDL4" class="btn btn-primary col-5">Открыть</a>
+                            <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                        </div>
+                    </div>
+                    <a href="https://www.youtube.com/watch?v=JtmZ2e6mDL4">
+                        <img src="https://img.youtube.com/vi/JtmZ2e6mDL4/maxresdefault.jpg" class="card-img-bottom" alt="...">
+                    </a>
+                </div>
+                <br>
+            </div>
+            <div class="tab-pane fade" id="articlelist" role="tabpanel" aria-labelledby="pills-contact-tab">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Введение в Data Science и машинное обучение</h5>
+                        <p class="card-text">Курс знакомит слушателей с основами машинного обучения и рассчитан в первую...</p>
+                        <div class="row justify-content-between">
+                            <a href="https://stepik.org/course/4852/promo" class="btn btn-primary col-5">Открыть</a>
+                            <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Представление налоговой и бухгалтерской отчётности</h5>
+                        <p class="card-text">ОБЩАЯ ИНФОРМАЦИЯ Налоговая отчетность включает в себя совокупность документов, ...</p>
+                        <p class="card-text"><small class="text-muted">≈ 4 мин (8 987 знаков)</small></p>
+                        <div class="row justify-content-between">
+                            <a href="https://www.nalog.ru/rn77/taxation/submission_statements/" class="btn btn-primary col-5">Открыть</a>
+                            <a href="#" class="btn btn-secondary col-5">Удалить</a>
+                        </div>
+                    </div>
+                </div>
+                <br>
+            </div>
+            <div class="tab-pane fade" id="musiclist" role="tabpanel" aria-labelledby="pills-contact-tab">
+
+            </div>
+            <div class="tab-pane fade" id="podcastlist" role="tabpanel" aria-labelledby="pills-contact-tab">
+
+            </div>
+            <div class="tab-pane fade" id="noteslist" role="tabpanel" aria-labelledby="pills-contact-tab">
+
             </div>
         </div>
-        <br>
 
     </div>
 </main>
